9d7fffff0475c02d027c8ec1785f2e7652c2416e
c2atech <c2atechltda@gmail.com>
2020-05-31 15:21:07 -0300

    galeria de fotos

--- resources/views/admin/galeria/index.blade.php
+++ resources/views/admin/galeria/index.blade.php
@@ -1,9 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<h1>
-    galeria de fotos
-</h1>
-</div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2>
+                            Postagens
+
+                            <a href="{{ url('admin/posts/create') }}" class="btn btn-default pull-right">Nova Postagem</a>
+                        </h2>
+                    </div>
+
+                    <div class="panel-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Imagem</th>
+                                    <th>Titulo</th>
+                                    <th>Ação</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    <tr>
+                                        <td><img height="100" width="100" src="#" alt=""></td>
+                                        <td></td>
+                                       
+                                        <td>
+                                          
+                                            <a href="#" class="btn btn-xs btn-success">Ver</a>
+                                            <a href="#") }}" class="btn btn-xs btn-info">Editar</a>
+                                            <a href="#" data-method="DELETE" data-token="{{ csrf_token() }}" data-confirm="Are you sure?" class="btn btn-xs btn-danger">Deletar</a>
+                                        </td>
+                                    </tr>
+                             
+                            </tbody>
+                        </table>
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
