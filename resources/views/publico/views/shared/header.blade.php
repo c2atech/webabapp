5f5d0231cf9c514b2b92ad9a598051ff23809c64
c2atech <c2atechltda@gmail.com>
2020-06-08 02:19:10 -0300

    Background

--- resources/views/publico/views/shared/header.blade.php
+++ resources/views/publico/views/shared/header.blade.php
@@ -4,7 +4,7 @@
         <div class="intro-text">
           <div class="intro-heading text-uppercase">ABAPP</div>
           <div class="intro-lead-in">Associação Beneficente Amor pelo Próximo</div>
-          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#noticias">Saiba mais</a>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#about">Saiba mais</a>
         </div>
       </div>
     </header>
