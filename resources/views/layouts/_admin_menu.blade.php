5211173eec083c8e5eea0cd423c9630692fa9c09
carlosheenrique <carlosheenrique.on@gmail.com>
2020-06-08 20:44:16 -0300

    Página Inicial

--- resources/views/layouts/_admin_menu.blade.php
+++ resources/views/layouts/_admin_menu.blade.php
@@ -1,7 +1,6 @@
 <ul class="nav navbar-nav">
     <li><a href="{{ url('admin/posts') }}">Postagens</a></li>
     <li><a href="{{ url('admin/categories') }}">Categorias</a></li>
-    <li><a href="{{ url('admin/comments') }}">Commentários</a></li>
     <li><a href="{{ url('admin/tags') }}">Tags</a></li>
     <li><a href="{{ url('adm/galeria') }}">Galeria</a></li>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,11 +16,14 @@
 
     <!-- Scripts -->
     <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
+        window.Laravel = {
+            !!json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!
+        };
     </script>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
@@ -35,8 +39,8 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Portal do ADM
+                    <a class="navbar-brand" href="{{ url('/home') }}">
+                        Home
                     </a>
                 </div>
 
@@ -49,28 +53,30 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('login') }}">Login</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{ url('/profile') }}">Profile</a></li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/') }}">Página Inicial</a></li>
+
+                                <li><a href="{{ url('/profile') }}">Profile</a></li>
+
+                                <li>
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+                                        Logout
+                                    </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
                         @endif
                     </ul>
                 </div>
@@ -89,4 +95,5 @@
     <!-- Scripts -->
     <script src="{{ asset('jsx/app.js') }}"></script>
 </body>
+
 </html>
